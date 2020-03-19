b0ec236edd09fa448f43635b110527c836da02a6
Philip <philip.carstensen@tuhh.de>
2020-03-19 11:45:32 +0100

    4dof mit Testkreis

---
+++ eigene Modelle/path_file_Modell4dof_Testkreis.m
@@ -0,0 +1,22 @@
+function [path]=path_file_Modell4dof_Testkreis(type,t,t_max,p0pf)
+  
+    % Daten für Kreisbahn
+    f = 0.85;
+    R = p0pf(1);
+    r = R*(1-f);
+    q = p0pf(2);
+    
+    %Path function
+    s = (t^5*(70*t^4 - 315*t^3*t_max + 540*t^2*t_max^2 - 420*t*t_max^3 + 126*t_max^4))/t_max^9;
+    zd=zeros(2,1);
+    
+   
+    if type==1
+        w = 2*pi; 
+        
+        zd = [cos(q)*f*R+cos(w*s+q)*r;...
+                 sin(q)*f*R+sin(w*s+q)*r];
+                 
+    end
+    path=zd;
+end
